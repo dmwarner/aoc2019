573d31236ee4ab91e1060521a29935c54dd2dc59
Dave Warner <dwarner1@hmc.psu.edu>
2019-12-07 13:52:50 -0500

    day02 problem 2 complete

--- day02/ProblemTwo.kt
+++ day02/ProblemTwo.kt
@@ -3,48 +3,61 @@ package aoc2019.day02
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("running 2019.day2.problem1")
+    println("running 2019.day2.problem 2")
 
+    var found = false
 
-    File("input.txt").forEachLine {
-        println("line ${it}")
+    File("day02/input.txt").forEachLine {
+        //println("line ${it}")
 
-        //val ops = it.split(",")
-        val ops = it.split(",").toTypedArray()
+        val ops = it.split(",").toTypedArray().map { it.toInt() }
         println("ops list contains ${ops.size} elements")
 
-        ops[1] = "12"
-        ops[2] = "2"
+        //val noun = 12
+        //val verb: Int = 2
 
+        for (noun: Int in 1..100) {
+            for (verb: Int in 1..100) {
+                val result = runProgram(ops.toMutableList(), noun, verb)
+                println("program result: $result for $noun : $verb")
+                //if (result == 2641045) {
+                if (result == 19690720) {
+                    println("FOUND ${(noun*100)+verb}")
+                    found = true
+                    break
 
-        for (i in 0..ops.size step 4) {
-            println("${i} operator is ${ops[i]}")
-            if (ops[i].toInt() == 99) {break}
-
-            //val op1: Int = ops[i+1].toInt()
-            //val op2: Int = ops[i+2].toInt()
-
-            when (ops[i].toInt()) {
-                1 -> {
-                    ops[ops[i+3].toInt()] = (ops[ops[i+1].toInt()].toInt() + ops[ops[i+2].toInt()].toInt()).toString()
-
-                }
-                2 -> {
-                    ops[ops[i+3].toInt()] = (ops[ops[i+1].toInt()].toInt() * ops[ops[i+2].toInt()].toInt()).toString()
                 }
             }
-            println("storing at ")
-            println("result ${println(ops.joinToString(","))}")
+            if (found) { break }
         }
-
     }
 
-
-    //println("ops list contains ${ops.size} elements")
 }
 
-fun calcFuel(mass: Int): Int {
-    val fuel: Int = mass.div(3).minus(2)
-    println("for mass ${mass}, need ${fuel} fuel")
-    return fuel
+fun runProgram(ops: MutableList<Int>, noun: Int, verb: Int): Int {
+    ops[1] = noun
+    ops[2] = verb
+
+    //println("running program ${ops}")
+    for (i in 0..ops.size step 4) {
+        //println("${i} operator is ${ops[i]}")
+        if (ops[i] == 99) {break}
+
+        //val op1: Int = ops[i+1].toInt()
+        //val op2: Int = ops[i+2].toInt()
+
+        when (ops[i]) {
+            1 -> {
+                ops[ops[i+3]] = (ops[ops[i+1]] + ops[ops[i+2]])
+
+            }
+            2 -> {
+                ops[ops[i+3]] = (ops[ops[i+1]] * ops[ops[i+2]])
+            }
+        }
+        //println("storing at ")
+        //println("result ${println(ops.joinToString(","))}")
+    }
+    return ops[0]
 }
+
